0656480a8bd8b8f959e8a43722a67dec6c81346d
GreywindGit <GreywindGit@users.noreply.github.com>
2017-10-23 00:33:04 +0200

    Add Person class

---
+++ CreateClass/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public enum Genders {Male, Female};
+        private Genders gender;
+
+        public Person(string name, DateTime birthDate, Genders gender)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+            this.gender = gender;
+        }
+
+        public Genders Gender { get { return gender; } }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Date of Birth: " + BirthDate.ToShortDateString() + ", Gender: " + gender.ToString();
+        }
+    }
+}
---
+++ CreateClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person John = new Person("John", new DateTime(1997, 10, 12), Person.Genders.Male);
+            Console.WriteLine(John.Name);
+            Console.WriteLine(John.BirthDate);
+            Console.WriteLine(John.Gender);
+            Console.WriteLine(John.ToString());
+            Console.ReadLine();
+        }
+    }
+}
