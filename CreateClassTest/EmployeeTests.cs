0a9eb086faab17eeac7634bfeb5b72841531bd5e
GreywindGit <GreywindGit@users.noreply.github.com>
2017-10-25 10:30:08 +0200

    Add test project + employee unit tests

---
+++ CreateClassTest/EmployeeTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CreateClass;
+
+namespace CreateClassTests
+{
+    [TestClass]
+    public class EmployeeTests
+    {
+        Employee Adam;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Adam = new Employee("Adam", new DateTime(1990, 5, 6), Person.Genders.Male, 2000, "QA");
+        }
+
+        [TestMethod]
+        public void TestEmployeeInitializationIsValid()
+        {
+            Assert.AreEqual("Adam", Adam.Name);
+            Assert.AreEqual(new DateTime(1990, 5, 6), Adam.BirthDate);
+            Assert.AreEqual(Person.Genders.Male, Adam.Gender);
+            Assert.AreEqual(2000, Adam.Salary);
+            Assert.AreEqual("QA", Adam.Profession);
+        }
+
+        [TestMethod]
+        public void TestEmployeeUpdateName()
+        {
+            Adam.Name = "Adam Jenssen";
+
+            var expected = "Adam Jenssen";
+            var actual = Adam.Name;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestEmployeeUpdateBirthDate()
+        {
+            Adam.BirthDate = new DateTime(1989, 5, 6);
+
+            var expected = new DateTime(1989, 5, 6);
+            var actual = Adam.BirthDate;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestEmployeeUpdateSalary()
+        {
+            Adam.Salary = 4000;
+
+            var expected = 4000;
+            var actual = Adam.Salary;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestEmployeeUpdateProfession()
+        {
+            Adam.Profession = "actor";
+
+            var expected = "actor";
+            var actual = Adam.Profession;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CreateClassTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CreateClassTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CreateClassTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6b67184-3c78-49d1-acc0-b31a4e1627a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
